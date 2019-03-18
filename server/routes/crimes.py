ec653c25461b8be928d48aea55c4df91e43ad198
Mohamed Raffik <mohamed@streetcred.co>
2019-03-17 23:59:01 -0400

    fixed formatting

--- server/routes/crimes.py
+++ server/routes/crimes.py
@@ -4,7 +4,7 @@ from server.models import db
 from sqlalchemy.sql import text, select, join
 from datetime import datetime, timedelta
 
- # CRIME_NAME
+# CRIME_NAME
 # Possible values:
 # ----
 
@@ -24,34 +24,34 @@ from datetime import datetime, timedelta
 
 
 def nearby_crimes():
-        lat = request.args.get('latitude')
-        lon = request.args.get('longitude')
-        crime_filter = tuple([ int(a) for a in request.args.getlist('filter') ])
-        crime_filter = tuple(range(1,13)) if len(crime_filter) == 0 else crime_filter
-        time_range = request.args.get('timeSpan')
+    lat = request.args.get('latitude')
+    lon = request.args.get('longitude')
+    crime_filter = tuple([ int(a) for a in request.args.getlist('filter') ])
+    crime_filter = tuple(range(1,13)) if len(crime_filter) == 0 else crime_filter
+    time_range = request.args.get('timeSpan')
 
-        today = datetime.today()
-        week = today - timedelta(days=7)
-        month = today - timedelta(days=30)
-        year = today - timedelta(days=365)
+    today = datetime.today()
+    week = today - timedelta(days=7)
+    month = today - timedelta(days=30)
+    year = today - timedelta(days=365)
 
-        with db.connect() as conn:
-                query = text(
-                        '''
-                        select t1.crime_date, t1.pd_desc, t1.latitude, t1.longitude, t2.category from crime_info as t1
-                        join crime_categories as t2 on t1.category_id = t2.id 
-                        where acos( sin( radians(:lat) ) * sin( radians(latitude) ) + cos( radians(:lat) ) * cos( radians(latitude) ) * cos( radians( :lon - longitude) ) ) * 6371 < 0.1524 and t1.category_id = ANY(:cat)
-                        '''
-                        )
-                results = conn.execute(query, lat=lat, lon=lon, cat=crime_filter).fetchall()
+    with db.connect() as conn:
+        query = text(
+            '''
+            select t1.crime_date, t1.pd_desc, t1.latitude, t1.longitude, t2.category from crime_info as t1
+            join crime_categories as t2 on t1.category_id = t2.id 
+            where acos( sin( radians(:lat) ) * sin( radians(latitude) ) + cos( radians(:lat) ) * cos( radians(latitude) ) * cos( radians( :lon - longitude) ) ) * 6371 < 0.1524 and t1.category_id = ANY(:cat)
+            '''
+        )
+        results = conn.execute(query, lat=lat, lon=lon, cat=crime_filter).fetchall()
 
-        return jsonify([
-                {
-                        "date": r[0],
-                        "crime_desc": r[1],
-                        "latitude": r[2],
-                        "longitude": r[3],
-                        "category": r[4],
-                }
-                for r in results
-        ])
+    return jsonify([
+        {
+            "date": r[0],
+            "crime_desc": r[1],
+            "latitude": r[2],
+            "longitude": r[3],
+            "category": r[4],
+        }
+        for r in results
+    ])
--- server/routes/stations.py
+++ server/routes/stations.py
@@ -3,26 +3,26 @@ from server.models import db
 from sqlalchemy.sql import text
 
 def nearby_stations():
-        lat = request.args.get('latitude')
-        lon = request.args.get('longitude')
-        limit = request.args.get('limit', 5)
+    lat = request.args.get('latitude')
+    lon = request.args.get('longitude')
+    limit = request.args.get('limit', 5)
 
-        with db.connect() as conn:
-                query = text(
-                        '''
-                        select * from (select name, line, latitude, longitude, acos( sin( radians(:lat) ) * sin( radians(latitude) ) + cos( radians(:lat) ) * cos( radians(latitude) ) * cos( radians(:lon - longitude) ) ) * 6371 as distance from stations) as t1
-                        where distance < 0.402336 limit :limit
-                        '''
-                        )
-                stations = conn.execute(query, lat=lat, lon=lon, limit=limit)
+    with db.connect() as conn:
+        query = text(
+            '''
+            select * from (select name, line, latitude, longitude, acos( sin( radians(:lat) ) * sin( radians(latitude) ) + cos( radians(:lat) ) * cos( radians(latitude) ) * cos( radians(:lon - longitude) ) ) * 6371 as distance from stations) as t1
+            where distance < 0.402336 limit :limit
+            '''
+        )
+        stations = conn.execute(query, lat=lat, lon=lon, limit=limit)
 
-        return [
-                {
-                        "name": station[0],
-                        "lines": station[1],
-                        "latitude": station[2],
-                        "longitude": station[3]
+    return jsonify([
+        {
+            "name": station[0],
+            "lines": station[1],
+            "latitude": station[2],
+            "longitude": station[3]
 
-                }
-                for station in stations
-        ]
+        }
+        for station in stations
+    ])

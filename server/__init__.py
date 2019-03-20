6b48637f6abc6646e21bcce3d1fe2d27010c9e5b
Mohamed Raffik <mohamed@streetcred.co>
2019-03-20 19:13:13 -0400

    added comments

--- server/__init__.py
+++ server/__init__.py
@@ -8,13 +8,17 @@ from flask_cors import CORS
 load_dotenv('.flaskenv')
 load_dotenv('.env')
 
+# Create Flask App and add OpenApi Documentation
 app = FlaskApp(__name__, specification_dir='./')
 app.add_api('openapi.yaml')
 
+# Add CORS to Flask App
 CORS(app.app)
+
 
 logger = logging.getLogger()
 
+# Default Route
 @app.route('/')
 @app.route('/api')
 def index():
